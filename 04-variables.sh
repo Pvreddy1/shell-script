5ea67b48a8243eccbd17114ecfb6681862a7598b
Pvreddy1 <pvreddy2286@gmail.com>
2025-01-04 18:51:32 +0530

    committed by 04-variable.sh

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
+echo "$PERSON3: Hello $PERSON1, AND $PERSON2, GOOD MORNING"
