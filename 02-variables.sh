3027de69f78e24ef16018da461bff90ebca25e04
Pvreddy1 <pvreddy2286@gmail.com>
2025-01-04 18:14:25 +0530

    committed by 02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -8,4 +8,4 @@ echo "$PERSON1: Hello $PERSON2, Good Morning"
 echo "$PERSON2: Hi $PERSON1, very Good Morning"
 echo "$PERSON1: How are you $PERSON2?"
 echo "$PERSON2: I am good $PERSON1, How are you"
-echo "$person3: Hello $PERSON1, AND $PERSON2, GOOD MORNING
+echo "$person3: Hello $PERSON1, AND $PERSON2, GOOD MORNING"
